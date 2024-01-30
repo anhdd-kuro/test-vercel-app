516052d3dbee0e63f7cea6cf07c5492a54082cc4
kuro <anhdd.kuro@gmail.com>
2024-01-31 00:05:35 +0900

    Add Shopify integration and error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import type { HeadersFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -5,11 +6,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useRouteError,
 } from "@remix-run/react";
+import { boundary } from "@shopify/shopify-app-remix";
 
 export default function App() {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
@@ -25,3 +28,12 @@ export default function App() {
     </html>
   );
 }
+
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
+
+export const headers: HeadersFunction = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
